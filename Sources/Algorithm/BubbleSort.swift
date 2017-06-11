4335c27bd34f5a04e851102189ef04b94f689d9d
Khoa Pham <onmyway133@gmail.com>
2017-06-11 16:46:18 +0200

    Update comment

--- Sources/Algorithm/BubbleSort.swift
+++ Sources/Algorithm/BubbleSort.swift
@@ -5,6 +5,8 @@ public extension Algorithm {
   // that repeatedly steps through the list to be sorted, compares each pair of adjacent items 
   // and swaps them if they are in the wrong order.
   // https://en.wikipedia.org/wiki/Bubble_sort
+  // After 1st iteration, biggest element moves to the end
+  // After 2nd iteration, 2nd biggest element moves next to the end
   public static func bubbleSort<T: Comparable>(items: [T]) -> [T] {
     var items = items
 
