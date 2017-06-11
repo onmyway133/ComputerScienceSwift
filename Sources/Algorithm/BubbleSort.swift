ab9d574a8cea4c3088c923045c4efe86925a265e
Khoa Pham <onmyway133@gmail.com>
2017-06-11 16:41:17 +0200

    Update bubbleSort

--- Sources/Algorithm/BubbleSort.swift
+++ Sources/Algorithm/BubbleSort.swift
@@ -1,10 +1,21 @@
 import Foundation
 
 public extension Algorithm {
-  /// Bubble sort is a simple and well-known sorting algorithm. 
-  /// It is used in practice once in a blue moon and its main application is to make an introduction to the sorting algorithms. 
-  /// Bubble sort belongs to O(n2) sorting algorithms, which makes it quite inefficient for sorting large data volumes. Bubble sort is stable and adaptive.
+  // Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm 
+  // that repeatedly steps through the list to be sorted, compares each pair of adjacent items 
+  // and swaps them if they are in the wrong order.
+  // https://en.wikipedia.org/wiki/Bubble_sort
   public static func bubbleSort<T: Comparable>(items: [T]) -> [T] {
-    return []
+    var items = items
+
+    for i in 0..<items.count-1 {
+      for j in 0..<items.count-1-i {
+        if items[j] > items[j+1] {
+          swap(&items[j], &items[j+1])
+        }
+      }
+    }
+
+    return items
   }
 }
